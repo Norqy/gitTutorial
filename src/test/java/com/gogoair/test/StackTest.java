49cd48e50b28400d460cc56a398b506930536635
Troy Mass <tmass@gogoair.com>
2013-11-22 13:48:14 -0600

    test case shouldPopFromStack

--- src/test/java/com/gogoair/test/StackTest.java
+++ src/test/java/com/gogoair/test/StackTest.java
@@ -1,5 +1,32 @@
 package com.gogoair.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.gogoair.tutorial.Stack;
+
 public class StackTest {
+	
+	@Test
+	public void shouldPopFromStack() {
+		final Integer I1 = new Integer(1);
+		final Integer I2 = new Integer(2);
+		final Integer I3 = new Integer(3);
+		
+		// given a stack containing data
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(I1);
+		s.push(I2);
+		s.push(I3);
+		
+		// when I call pop
+		Integer ret = s.pop();
+		
+		// then I should see the last item inserted
+		assertEquals(I3,ret);
+		// and size should decrease
+		assertEquals(2,s.size());
+	}
 	
 }
