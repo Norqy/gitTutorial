bc9a32d6fec0737fb2505c6a3e654316e7ee1511
Troy Mass <tmass@gogoair.com>
2013-11-22 14:22:37 -0600

    Implimented Stack code

--- src/main/java/com/gogoair/tutorial/Stack.java
+++ src/main/java/com/gogoair/tutorial/Stack.java
@@ -1,22 +1,45 @@
 package com.gogoair.tutorial;
 
 public class Stack<E> {
+	private class Node {
+		public E data;
+		public Node next;
+		
+		public Node(E data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+	private int size;
+	private Node top;
 	
 	public Stack() {
+		this.size = 0;
+		this.top = null;
 	}
 	
 	public void push(E data) {
+		top = new Node(data, top);
+		this.size++;
 	}
 	
 	public E pop() {
+		if (top != null) {
+			E data = top.data;
+			top = top.next;
+			this.size--;
+			return data;
+		}
 		return null;
 	}
 	
 	public E top() {
+		if (top != null)
+			return top.data;
 		return null;
 	}
 	
 	public int size() {
-		return 0;
+		return this.size;
 	}
 }
