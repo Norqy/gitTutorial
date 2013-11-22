c8890b329e542d66064c816353350ada14efcb8c
Troy Mass <tmass@gogoair.com>
2013-11-22 13:40:47 -0600

    Stub for Stack

---
+++ src/main/java/com/gogoair/tutorial/Stack.java
@@ -0,0 +1,22 @@
+package com.gogoair.tutorial;
+
+public class Stack<E> {
+	
+	public Stack() {
+	}
+	
+	public void push(E data) {
+	}
+	
+	public E pop() {
+		return null;
+	}
+	
+	public E top() {
+		return null;
+	}
+	
+	public int size() {
+		return 0;
+	}
+}
